5f23e1c5f66ddf29ac9a6f2f508e286d38542ab5
Aman Dwivedi <g25.aman.krishnadwivedi@gnkhalsa.edu.in>
2026-04-03 02:06:01 +0530

    text to textnode func add and test

---
+++ src/tests/test_text_to_textnodes.py
@@ -0,0 +1,32 @@
+from test_to_textnodes import text_to_textnodes
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextToTextNode(unittest.TestCase):
+    def test_eq(self):
+        text_string = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        node = text_to_textnodes(text_string)
+
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode(
+                    "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+                ),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            node,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
